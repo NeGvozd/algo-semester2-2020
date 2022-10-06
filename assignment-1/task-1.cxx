658cca8185117cfdae067b002a6aa2f348638435
Nikita Gvozdkov <gvozdkov.na@phystech.edu>
2022-10-06 16:41:48 +0300

    task 1 in assigment 1 is solved

--- assignment-1/task-1.cxx
+++ assignment-1/task-1.cxx
@@ -29,9 +29,76 @@
 ****************************************************************************/
 
 #include <iostream>
+#include <vector>
+
+
+std::vector<int> bfs(const std::vector<std::vector<int>>& graph, const int start) 
+{
+    std::vector<int> dist(graph.size(), -1);
+    std::vector<int> queue;
+
+    queue.push_back(start);
+    dist[start] = 0;
+
+    for (size_t i = 0; i < queue.size(); ++i) {
+        int v = queue[i];
+
+        for (size_t j = 0; j < graph[v].size(); ++j) {
+            int to = graph[v][j];
+
+            if (dist[to] == -1) {
+                dist[to] = dist[v] + 1;
+                queue.push_back(to);
+            }
+        }
+    }
+
+    return dist;
+}
+
+
+int findMinPath(const std::vector<std::vector<int>>& graph, 
+                const int leon, 
+                const int matilda, 
+                const int milk)
+{
+    std::vector<int> dist_leon = bfs(graph, leon);
+    std::vector<int> dist_matilda = bfs(graph, matilda);
+    std::vector<int> dist_milk = bfs(graph, milk);
+
+    int min_path = dist_leon[milk] + dist_matilda[milk];
+
+    for (size_t i = 0; i < graph.size(); ++i) {
+        if (dist_leon[i] != -1 && dist_matilda[i] != -1 && 
+                dist_milk[i] != -1) {
+            int path = dist_leon[i] + dist_matilda[i] + dist_milk[i];
+            if (path < min_path) {
+                min_path = path;
+            }
+        }
+    }
+
+    return min_path;
+}
+
 
 int main()
 {
+    int vertex_count, edge_count, leon, matilda, milk;
+    std::cin >> vertex_count >> edge_count >> leon >> matilda >> milk;
+
+    std::vector<std::vector<int>> graph(vertex_count + 1);
+    for (int i = 0; i < edge_count; ++i) {
+        int from, to;
+
+        std::cin >> from >> to;
+
+        graph[from].push_back(to);
+        graph[to].push_back(from);
+    }
+
+    std::cout << findMinPath(graph, leon, matilda, milk) << std::endl;
+
     return 0;
 }
 
