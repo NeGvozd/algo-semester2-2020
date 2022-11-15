354d1a1d0c736d86a0476b0ae13b2a71c2acb0ff
Nikita Gvozdkov <gvozdkov.na@phystech.edu>
2022-11-16 00:33:17 +0300

    task 3 in assigment1

--- assignment-1/task-3.cxx
+++ assignment-1/task-3.cxx
@@ -37,8 +37,109 @@
 #include <vector>
 #include <algorithm>
 
+
 enum class Color { White, Gray, Black };
 
+
+void dfs1(const std::vector<std::vector<int>>& graph, 
+          std::vector<Color>& colors, 
+          std::vector<int>& order, 
+          int vertex) 
+{
+    colors[vertex] = Color::Gray;
+    for (int v : graph[vertex])
+    {
+        if (colors[v] == Color::White) {
+            dfs1(graph, colors, order, v);
+        }
+    }
+    colors[vertex] = Color::Black;
+    order.push_back(vertex);
+}
+
+
+void dfs2(const std::vector<std::vector<int>>& graph,
+          std::vector<std::vector<int>>& components,
+          std::vector<Color>& colors,
+          int vertex,
+          int component)
+{
+    colors[vertex] = Color::Gray;
+    components[component].push_back(vertex);
+    for (int v : graph[vertex])
+    {
+        if (colors[v] == Color::White) {
+            dfs2(graph, components, colors, v, component);
+        }
+    }
+    colors[vertex] = Color::Black;
+}
+
+
+void FindComponents(const std::vector<std::vector<int>>& graph,
+                    std::vector<std::vector<int>>& components)
+{
+    std::vector<Color> colors(graph.size(), Color::White);
+    std::vector<int> order;
+    for (int i = 0; i < graph.size(); ++i)
+    {
+        if (colors[i] == Color::White) {
+            dfs1(graph, colors, order, i);
+        }
+    }
+
+    int component = 0;
+    colors.assign(graph.size(), Color::White);
+
+    for (int i = graph.size() - 1; i >= 0; --i)
+    {
+        if (colors[order[i]] == Color::White) {
+            components.push_back(std::vector<int>());
+            dfs2(graph, components, colors, order[i], component);
+            ++component;
+        }
+    }
+}
+
+
+int AdditionalRoads(const std::vector<std::vector<int>>& graph)
+{
+    std::vector<std::vector<int>> components;
+    FindComponents(graph, components);
+
+    if (components.size() == 1) {
+        return 0;
+    }
+
+    std::vector<bool> is_incoming(components.size(), false);
+    std::vector<bool> is_outgoing(components.size(), false);
+
+    for (int u = 0; u < graph.size(); ++u)
+    {
+        for (int v : graph[u])
+        {
+            if (v != u) {
+                is_outgoing[u] = true;
+                is_incoming[v] = true;
+            }
+        }
+    }
+
+    int incoming = 0;
+    int outgoing = 0;
+
+    for (int i = 0; i < components.size(); ++i)
+    {
+        if (is_incoming[i]) {
+            ++incoming;
+        }
+        if (is_outgoing[i]) {
+            ++outgoing;
+        }
+    }
+
+    return std::max(incoming, outgoing);
+}
 
 
 int main()
@@ -54,9 +155,6 @@ int main()
         graph[a].push_back(b);
     }
 
-    std::vector<Color> colors(n, Color::White);
-    
-    
+    std::cout << AdditionalRoads(graph) << std::endl;
     return 0;
 }
-
